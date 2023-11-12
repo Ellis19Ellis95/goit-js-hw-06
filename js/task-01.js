21c596b46c2e933f8ce6c2bd82a599e4e8c02811
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-12 20:13:05 +0200

    3

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const categoriesList = document.getElementById('categories');          
 const categoryItems = categoriesList.querySelectorAll('li.item');
-console.log (Number of categories: ${categoryItems.length});
+console.log(Number of categories: ${categoryItems.length});
 categoryItems.forEach((categoryItem) => {
     const categoryTitle = categoryItem.querySelector('h2').textContent;
     const categoryElements = categoryItem.querySelectorAll('ul li');
